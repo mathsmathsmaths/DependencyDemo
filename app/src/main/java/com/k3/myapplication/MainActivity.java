9726645cdecd35e186436af0dbc6ca96767bb4b2
khoa2nguyen <k3.nguyen.family@gmail.com>
2019-07-01 21:05:50 -0500

    Initial commit

---
+++ app/src/main/java/com/k3/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.k3.myapplication;
+
+import com.android.support.v4.app.ActivityCompat;
+import com.android.support.v4.app.Fragment;
+import com.android.support.v4.app.FragmentManager;
+import com.android.support.v4.app.FragmentTransaction;
+import com.android.support.v4.app.AppCompatActivity;
+
+
+
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends ActivityCompat {
+
+}
